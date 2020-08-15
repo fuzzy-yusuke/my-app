a489dee1d2a5f8cbf27734e65b32c0b869caf03f
fuzzy-yusuke <youth_k0228@outlook.com>
2020-08-15 16:01:19 +0900

    function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {    //classで定義付けるより、functionで定義した方が記述が少ない？
+    return(
+      <List />
+    );
+  
 }
 
 export default App;
