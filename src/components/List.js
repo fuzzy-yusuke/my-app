afb04cc751ab4089a32e05939d038a47c3d13d03
fuzzy-yusuke <youth_k0228@outlook.com>
2020-08-15 17:56:17 +0900

    stateを追加

--- src/components/List.js
+++ src/components/List.js
@@ -2,15 +2,37 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+  
+  constructor(props){   //props...プロパティの事。ここではComponentクラスのプロパティを受け取っている
+    super(props);       //親クラス(Component)からプロパティを引き継いでいる
+    this.state={
+      todos:[           //コンポーネント内で使用する値
+        '掃除する',
+        '買い物',
+        '洗濯する'
+      ]
+    };
+  }
+  onClickAdd() {
+    const newTodo = window.prompt("やることを入力してください", "");
+    const todos = this.state.todos;
+    todos.push(newTodo);
+    this.setState({todos});
+  }
+
   render() {
     return (
-      <table>
-        <tbody>
-          <tr><td style={{border: 1, borderColor: 'gray', borderStyle: 'solid', cursor: 'pointer'}}>掃除する</td></tr>
-          <tr><td style={{border: 1, borderColor: 'gray', borderStyle: 'solid', cursor: 'pointer'}}>買い物</td></tr>
-          <tr><td style={{border: 1, borderColor: 'gray', borderStyle: 'solid', cursor: 'pointer'}}>洗濯する</td></tr>
-        </tbody>
-      </table>
+      <div>
+        <div><button onClick={() => this.onClickAdd()}>追加</button></div>
+        <table>
+          <tbody>
+          {this.state.todos.map((todo) => (
+            <tr key={todo}><td style={{border: 1, borderColor: 'gray', borderStyle: 'solid', cursor: 'pointer'}}>{todo}</td></tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
+
     );
   }
 }
